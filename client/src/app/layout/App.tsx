57b7c4ebab9cd977ed64c345fb40bf25e3b70d45
Benito Pedro <benitopedro21@outlook.com>
2022-05-07 15:46:27 -0300

    passando controle de estado para o componente pai

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import Catalog from '../../features/Catalog';
+import { Product } from '../models/product';
 
 function App() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7064/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div>
       <h1>Re-store</h1>
-      <Catalog />
+      <Catalog products={products} setProducts={setProducts} />
     </div>
   );
 }
--- client/src/features/Catalog.tsx
+++ client/src/features/Catalog.tsx
@@ -1,15 +1,7 @@
-import { useState, useEffect } from "react";
 import { Product } from "../app/models/product";
+import { CatalogComponent } from "../app/models/catalogComponent"
 
-const Catalog = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("https://localhost:7064/api/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((err) => console.error(err));
-  }, []);
+const Catalog = ({ products, setProducts }: CatalogComponent) => {
 
   const addProduct = () => {
     const newProduct: Product = {
